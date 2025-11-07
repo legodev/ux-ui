3140ea5b15ab607413b58dfb3b3022309cb81356
legodev <gonzalezleonardo283@gmail.com>
2025-11-06 21:52:44 -0300

    Initial project

---
+++ html/html1/README.md
@@ -0,0 +1,2 @@
+# proyecto-ux-ui
+Proyecto 6 de la materia UX/UI
---
+++ script.js
@@ -0,0 +1,5 @@
+const year = document.getElementById('year')
+
+let date = new Date()
+
+year.innerText = '2025-' + date.getFullYear() + ' Leonardo González'
